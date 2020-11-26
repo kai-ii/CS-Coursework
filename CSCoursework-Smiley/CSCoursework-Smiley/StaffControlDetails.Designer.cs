013913c5d73ea0947d6f26d9eafc78cf23a2e625
Kai Chevannes (Student) <kc314508@callywith.ac.uk>
2020-11-26 10:05:19 +0000

    All staff values now show in staff control details

--- CSCoursework-Smiley/CSCoursework-Smiley/StaffControlDetails.Designer.cs
+++ CSCoursework-Smiley/CSCoursework-Smiley/StaffControlDetails.Designer.cs
@@ -29,15 +29,32 @@
         private void InitializeComponent()
         {
             this.grpAddress = new System.Windows.Forms.GroupBox();
+            this.lblAddressPostcode = new System.Windows.Forms.Label();
+            this.lblAddressCounty = new System.Windows.Forms.Label();
+            this.lblAddressCity = new System.Windows.Forms.Label();
+            this.lblAddressStreet = new System.Windows.Forms.Label();
             this.grpEmployment = new System.Windows.Forms.GroupBox();
+            this.lblEmploymentBranch = new System.Windows.Forms.Label();
+            this.lblEmploymentContractType = new System.Windows.Forms.Label();
+            this.lblEmploymentJobTitle = new System.Windows.Forms.Label();
+            this.lblEmploymentStaffID = new System.Windows.Forms.Label();
             this.grpContactInfo = new System.Windows.Forms.GroupBox();
             this.grpEmploymentInfo = new System.Windows.Forms.GroupBox();
             this.grpPaymentDetails = new System.Windows.Forms.GroupBox();
-            this.lblAddressStreet = new System.Windows.Forms.Label();
-            this.lblAddressCity = new System.Windows.Forms.Label();
-            this.lblAddressCounty = new System.Windows.Forms.Label();
-            this.lblAddressPostcode = new System.Windows.Forms.Label();
+            this.lblContactInfoEmailAddress = new System.Windows.Forms.Label();
+            this.lblContactInfoMobileNumber = new System.Windows.Forms.Label();
+            this.lblContactInfoHomeNumber = new System.Windows.Forms.Label();
+            this.lblEmploymentInfoDoB = new System.Windows.Forms.Label();
+            this.lblEmploymentInfoGender = new System.Windows.Forms.Label();
+            this.lblEmploymentInfoCurrentlyEmployed = new System.Windows.Forms.Label();
+            this.lblPaymentDetailsNILetter = new System.Windows.Forms.Label();
+            this.lblPaymentDetailsTaxCode = new System.Windows.Forms.Label();
+            this.lblPaymentDetailsWorksNumber = new System.Windows.Forms.Label();
             this.grpAddress.SuspendLayout();
+            this.grpEmployment.SuspendLayout();
+            this.grpContactInfo.SuspendLayout();
+            this.grpEmploymentInfo.SuspendLayout();
+            this.grpPaymentDetails.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpAddress
@@ -54,8 +71,48 @@
             this.grpAddress.TabStop = false;
             this.grpAddress.Text = "Address";
             // 
+            // lblAddressPostcode
+            // 
+            this.lblAddressPostcode.AutoSize = true;
+            this.lblAddressPostcode.Location = new System.Drawing.Point(7, 82);
+            this.lblAddressPostcode.Name = "lblAddressPostcode";
+            this.lblAddressPostcode.Size = new System.Drawing.Size(80, 20);
+            this.lblAddressPostcode.TabIndex = 3;
+            this.lblAddressPostcode.Text = "postcode";
+            // 
+            // lblAddressCounty
+            // 
+            this.lblAddressCounty.AutoSize = true;
+            this.lblAddressCounty.Location = new System.Drawing.Point(6, 62);
+            this.lblAddressCounty.Name = "lblAddressCounty";
+            this.lblAddressCounty.Size = new System.Drawing.Size(60, 20);
+            this.lblAddressCounty.TabIndex = 2;
+            this.lblAddressCounty.Text = "county";
+            // 
+            // lblAddressCity
+            // 
+            this.lblAddressCity.AutoSize = true;
+            this.lblAddressCity.Location = new System.Drawing.Point(7, 42);
+            this.lblAddressCity.Name = "lblAddressCity";
+            this.lblAddressCity.Size = new System.Drawing.Size(35, 20);
+            this.lblAddressCity.TabIndex = 1;
+            this.lblAddressCity.Text = "city";
+            // 
+            // lblAddressStreet
+            // 
+            this.lblAddressStreet.AutoSize = true;
+            this.lblAddressStreet.Location = new System.Drawing.Point(7, 22);
+            this.lblAddressStreet.Name = "lblAddressStreet";
+            this.lblAddressStreet.Size = new System.Drawing.Size(50, 20);
+            this.lblAddressStreet.TabIndex = 0;
+            this.lblAddressStreet.Text = "street";
+            // 
             // grpEmployment
             // 
+            this.grpEmployment.Controls.Add(this.lblEmploymentBranch);
+            this.grpEmployment.Controls.Add(this.lblEmploymentContractType);
+            this.grpEmployment.Controls.Add(this.lblEmploymentJobTitle);
+            this.grpEmployment.Controls.Add(this.lblEmploymentStaffID);
             this.grpEmployment.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpEmployment.Location = new System.Drawing.Point(3, 120);
             this.grpEmployment.Name = "grpEmployment";
@@ -64,8 +121,47 @@
             this.grpEmployment.TabStop = false;
             this.grpEmployment.Text = "Employment";
             // 
+            // lblEmploymentBranch
+            // 
+            this.lblEmploymentBranch.AutoSize = true;
+            this.lblEmploymentBranch.Location = new System.Drawing.Point(6, 82);
+            this.lblEmploymentBranch.Name = "lblEmploymentBranch";
+            this.lblEmploymentBranch.Size = new System.Drawing.Size(62, 20);
+            this.lblEmploymentBranch.TabIndex = 7;
+            this.lblEmploymentBranch.Text = "branch";
+            // 
+            // lblEmploymentContractType
+            // 
+            this.lblEmploymentContractType.AutoSize = true;
+            this.lblEmploymentContractType.Location = new System.Drawing.Point(6, 62);
+            this.lblEmploymentContractType.Name = "lblEmploymentContractType";
+            this.lblEmploymentContractType.Size = new System.Drawing.Size(110, 20);
+            this.lblEmploymentContractType.TabIndex = 6;
+            this.lblEmploymentContractType.Text = "contract type";
+            // 
+            // lblEmploymentJobTitle
+            // 
+            this.lblEmploymentJobTitle.AutoSize = true;
+            this.lblEmploymentJobTitle.Location = new System.Drawing.Point(6, 42);
+            this.lblEmploymentJobTitle.Name = "lblEmploymentJobTitle";
+            this.lblEmploymentJobTitle.Size = new System.Drawing.Size(62, 20);
+            this.lblEmploymentJobTitle.TabIndex = 5;
+            this.lblEmploymentJobTitle.Text = "job title";
+            // 
+            // lblEmploymentStaffID
+            // 
+            this.lblEmploymentStaffID.AutoSize = true;
+            this.lblEmploymentStaffID.Location = new System.Drawing.Point(6, 22);
+            this.lblEmploymentStaffID.Name = "lblEmploymentStaffID";
+            this.lblEmploymentStaffID.Size = new System.Drawing.Size(57, 20);
+            this.lblEmploymentStaffID.TabIndex = 4;
+            this.lblEmploymentStaffID.Text = "staff id";
+            // 
             // grpContactInfo
             // 
+            this.grpContactInfo.Controls.Add(this.lblContactInfoHomeNumber);
+            this.grpContactInfo.Controls.Add(this.lblContactInfoMobileNumber);
+            this.grpContactInfo.Controls.Add(this.lblContactInfoEmailAddress);
             this.grpContactInfo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpContactInfo.Location = new System.Drawing.Point(4, 237);
             this.grpContactInfo.Name = "grpContactInfo";
@@ -76,6 +172,9 @@
             // 
             // grpEmploymentInfo
             // 
+            this.grpEmploymentInfo.Controls.Add(this.lblEmploymentInfoCurrentlyEmployed);
+            this.grpEmploymentInfo.Controls.Add(this.lblEmploymentInfoGender);
+            this.grpEmploymentInfo.Controls.Add(this.lblEmploymentInfoDoB);
             this.grpEmploymentInfo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpEmploymentInfo.Location = new System.Drawing.Point(3, 354);
             this.grpEmploymentInfo.Name = "grpEmploymentInfo";
@@ -86,6 +185,9 @@
             // 
             // grpPaymentDetails
             // 
+            this.grpPaymentDetails.Controls.Add(this.lblPaymentDetailsWorksNumber);
+            this.grpPaymentDetails.Controls.Add(this.lblPaymentDetailsTaxCode);
+            this.grpPaymentDetails.Controls.Add(this.lblPaymentDetailsNILetter);
             this.grpPaymentDetails.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grpPaymentDetails.Location = new System.Drawing.Point(4, 471);
             this.grpPaymentDetails.Name = "grpPaymentDetails";
@@ -94,41 +196,86 @@
             this.grpPaymentDetails.TabStop = false;
             this.grpPaymentDetails.Text = "Payment Details";
             // 
-            // lblAddressStreet
+            // lblContactInfoEmailAddress
             // 
-            this.lblAddressStreet.AutoSize = true;
-            this.lblAddressStreet.Location = new System.Drawing.Point(7, 26);
-            this.lblAddressStreet.Name = "lblAddressStreet";
-            this.lblAddressStreet.Size = new System.Drawing.Size(50, 20);
-            this.lblAddressStreet.TabIndex = 0;
-            this.lblAddressStreet.Text = "street";
+            this.lblContactInfoEmailAddress.AutoSize = true;
+            this.lblContactInfoEmailAddress.Location = new System.Drawing.Point(6, 22);
+            this.lblContactInfoEmailAddress.Name = "lblContactInfoEmailAddress";
+            this.lblContactInfoEmailAddress.Size = new System.Drawing.Size(109, 20);
+            this.lblContactInfoEmailAddress.TabIndex = 8;
+            this.lblContactInfoEmailAddress.Text = "email address";
             // 
-            // lblAddressCity
+            // lblContactInfoMobileNumber
             // 
-            this.lblAddressCity.AutoSize = true;
-            this.lblAddressCity.Location = new System.Drawing.Point(7, 46);
-            this.lblAddressCity.Name = "lblAddressCity";
-            this.lblAddressCity.Size = new System.Drawing.Size(35, 20);
-            this.lblAddressCity.TabIndex = 1;
-            this.lblAddressCity.Text = "city";
+            this.lblContactInfoMobileNumber.AutoSize = true;
+            this.lblContactInfoMobileNumber.Location = new System.Drawing.Point(6, 42);
+            this.lblContactInfoMobileNumber.Name = "lblContactInfoMobileNumber";
+            this.lblContactInfoMobileNumber.Size = new System.Drawing.Size(118, 20);
+            this.lblContactInfoMobileNumber.TabIndex = 9;
+            this.lblContactInfoMobileNumber.Text = "mobile number";
             // 
-            // lblAddressCounty
+            // lblContactInfoHomeNumber
             // 
-            this.lblAddressCounty.AutoSize = true;
-            this.lblAddressCounty.Location = new System.Drawing.Point(7, 66);
-            this.lblAddressCounty.Name = "lblAddressCounty";
-            this.lblAddressCounty.Size = new System.Drawing.Size(60, 20);
-            this.lblAddressCounty.TabIndex = 2;
-            this.lblAddressCounty.Text = "county";
+            this.lblContactInfoHomeNumber.AutoSize = true;
+            this.lblContactInfoHomeNumber.Location = new System.Drawing.Point(6, 62);
+            this.lblContactInfoHomeNumber.Name = "lblContactInfoHomeNumber";
+            this.lblContactInfoHomeNumber.Size = new System.Drawing.Size(111, 20);
+            this.lblContactInfoHomeNumber.TabIndex = 10;
+            this.lblContactInfoHomeNumber.Text = "home number";
             // 
-            // lblAddressPostcode
+            // lblEmploymentInfoDoB
             // 
-            this.lblAddressPostcode.AutoSize = true;
-            this.lblAddressPostcode.Location = new System.Drawing.Point(7, 86);
-            this.lblAddressPostcode.Name = "lblAddressPostcode";
-            this.lblAddressPostcode.Size = new System.Drawing.Size(80, 20);
-            this.lblAddressPostcode.TabIndex = 3;
-            this.lblAddressPostcode.Text = "postcode";
+            this.lblEmploymentInfoDoB.AutoSize = true;
+            this.lblEmploymentInfoDoB.Location = new System.Drawing.Point(7, 22);
+            this.lblEmploymentInfoDoB.Name = "lblEmploymentInfoDoB";
+            this.lblEmploymentInfoDoB.Size = new System.Drawing.Size(99, 20);
+            this.lblEmploymentInfoDoB.TabIndex = 11;
+            this.lblEmploymentInfoDoB.Text = "date of birth";
+            // 
+            // lblEmploymentInfoGender
+            // 
+            this.lblEmploymentInfoGender.AutoSize = true;
+            this.lblEmploymentInfoGender.Location = new System.Drawing.Point(7, 42);
+            this.lblEmploymentInfoGender.Name = "lblEmploymentInfoGender";
+            this.lblEmploymentInfoGender.Size = new System.Drawing.Size(63, 20);
+            this.lblEmploymentInfoGender.TabIndex = 12;
+            this.lblEmploymentInfoGender.Text = "gender";
+            // 
+            // lblEmploymentInfoCurrentlyEmployed
+            // 
+            this.lblEmploymentInfoCurrentlyEmployed.AutoSize = true;
+            this.lblEmploymentInfoCurrentlyEmployed.Location = new System.Drawing.Point(7, 62);
+            this.lblEmploymentInfoCurrentlyEmployed.Name = "lblEmploymentInfoCurrentlyEmployed";
+            this.lblEmploymentInfoCurrentlyEmployed.Size = new System.Drawing.Size(151, 20);
+            this.lblEmploymentInfoCurrentlyEmployed.TabIndex = 13;
+            this.lblEmploymentInfoCurrentlyEmployed.Text = "currently employed";
+            // 
+            // lblPaymentDetailsNILetter
+            // 
+            this.lblPaymentDetailsNILetter.AutoSize = true;
+            this.lblPaymentDetailsNILetter.Location = new System.Drawing.Point(6, 22);
+            this.lblPaymentDetailsNILetter.Name = "lblPaymentDetailsNILetter";
+            this.lblPaymentDetailsNILetter.Size = new System.Drawing.Size(67, 20);
+            this.lblPaymentDetailsNILetter.TabIndex = 14;
+            this.lblPaymentDetailsNILetter.Text = "NI letter";
+            // 
+            // lblPaymentDetailsTaxCode
+            // 
+            this.lblPaymentDetailsTaxCode.AutoSize = true;
+            this.lblPaymentDetailsTaxCode.Location = new System.Drawing.Point(6, 42);
+            this.lblPaymentDetailsTaxCode.Name = "lblPaymentDetailsTaxCode";
+            this.lblPaymentDetailsTaxCode.Size = new System.Drawing.Size(75, 20);
+            this.lblPaymentDetailsTaxCode.TabIndex = 15;
+            this.lblPaymentDetailsTaxCode.Text = "tax code";
+            // 
+            // lblPaymentDetailsWorksNumber
+            // 
+            this.lblPaymentDetailsWorksNumber.AutoSize = true;
+            this.lblPaymentDetailsWorksNumber.Location = new System.Drawing.Point(6, 62);
+            this.lblPaymentDetailsWorksNumber.Name = "lblPaymentDetailsWorksNumber";
+            this.lblPaymentDetailsWorksNumber.Size = new System.Drawing.Size(112, 20);
+            this.lblPaymentDetailsWorksNumber.TabIndex = 16;
+            this.lblPaymentDetailsWorksNumber.Text = "works number";
             // 
             // StaffControlDetails
             // 
@@ -144,6 +291,14 @@
             this.Load += new System.EventHandler(this.StaffControlDetails_Load);
             this.grpAddress.ResumeLayout(false);
             this.grpAddress.PerformLayout();
+            this.grpEmployment.ResumeLayout(false);
+            this.grpEmployment.PerformLayout();
+            this.grpContactInfo.ResumeLayout(false);
+            this.grpContactInfo.PerformLayout();
+            this.grpEmploymentInfo.ResumeLayout(false);
+            this.grpEmploymentInfo.PerformLayout();
+            this.grpPaymentDetails.ResumeLayout(false);
+            this.grpPaymentDetails.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -159,5 +314,18 @@
         private System.Windows.Forms.Label lblAddressCounty;
         private System.Windows.Forms.Label lblAddressCity;
         private System.Windows.Forms.Label lblAddressStreet;
+        private System.Windows.Forms.Label lblEmploymentStaffID;
+        private System.Windows.Forms.Label lblEmploymentJobTitle;
+        private System.Windows.Forms.Label lblEmploymentContractType;
+        private System.Windows.Forms.Label lblEmploymentBranch;
+        private System.Windows.Forms.Label lblContactInfoHomeNumber;
+        private System.Windows.Forms.Label lblContactInfoMobileNumber;
+        private System.Windows.Forms.Label lblContactInfoEmailAddress;
+        private System.Windows.Forms.Label lblEmploymentInfoDoB;
+        private System.Windows.Forms.Label lblEmploymentInfoCurrentlyEmployed;
+        private System.Windows.Forms.Label lblEmploymentInfoGender;
+        private System.Windows.Forms.Label lblPaymentDetailsWorksNumber;
+        private System.Windows.Forms.Label lblPaymentDetailsTaxCode;
+        private System.Windows.Forms.Label lblPaymentDetailsNILetter;
     }
 }
--- CSCoursework-Smiley/CSCoursework-Smiley/StaffControlDetails.cs
+++ CSCoursework-Smiley/CSCoursework-Smiley/StaffControlDetails.cs
@@ -8,10 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.OleDb;
+
 namespace CSCoursework_Smiley
 {
     public partial class StaffControlDetails : UserControl
     {
+        //Initialise variables
+        OleDbConnection con = new OleDbConnection();
+
+        //Initialise dict
         Dictionary<string, string> staffInfoDict = new Dictionary<string, string>();
         public DataRow staff_details 
         {
@@ -38,6 +44,8 @@ namespace CSCoursework_Smiley
                 staffInfoDict.Add("staff_home_number", value.Field<string>("staff_home_number"));
                 staffInfoDict.Add("staff_email_address", value.Field<string>("staff_email_address"));
                 staffInfoDict.Add("staff_employed", Convert.ToString(value.Field<bool>("staff_employed")));
+                InitializeDatabaseConnection();
+                staffInfoDict.Add("jobposition_name", GetJobPositionName(Convert.ToInt32(staffInfoDict["jobposition_id"])));
                 UpdateUserDetails();
             }
         }
@@ -51,12 +59,75 @@ namespace CSCoursework_Smiley
 
         }
 
+        private string GetJobPositionName(int jobposition_id)
+        {
+            //Initialize variables
+            DataSet JobPositionDS;
+            OleDbDataAdapter da;
+            string sql;
+
+            //Check Login Details
+            sql = $"SELECT * FROM tblJobPositions WHERE jobposition_id={jobposition_id}";
+            da = new OleDbDataAdapter(sql, con);
+            JobPositionDS = new DataSet();
+            da.Fill(JobPositionDS, "JobPosition");
+
+            return Convert.ToString(JobPositionDS.Tables["JobPosition"].Rows[0].Field<string>("jobposition_name"));
+        }
+
+        private void InitializeDatabaseConnection()
+        {
+            //Initialize variables
+            string dbProvider;
+            string DatabasePath;
+            string CurrentProjectPath;
+            string FormattedDatabasePath;
+            string FullDatabasePath;
+            string dbSource;
+
+            try
+            {
+                //Establish Connection with Database
+                dbProvider = "PROVIDER=Microsoft.ACE.OLEDB.12.0;";
+                DatabasePath = "/TestDatabase.accdb";
+                CurrentProjectPath = System.AppDomain.CurrentDomain.BaseDirectory;
+                FormattedDatabasePath = CurrentProjectPath.Remove(CurrentProjectPath.Length - 31, 31); //Cuts off the last 31 chars which gives the directory which the database is located
+                FullDatabasePath = FormattedDatabasePath + DatabasePath;
+                dbSource = "Data Source =" + FullDatabasePath;
+                con.ConnectionString = dbProvider + dbSource;
+                con.Open();
+                Console.WriteLine("Connection established");
+            }
+            catch
+            {
+                MessageBox.Show("Error establishing database connection.");
+            }
+        }
+
         private void UpdateUserDetails()
         {
+            //Address
             lblAddressStreet.Text = staffInfoDict["staff_street"];
             lblAddressCity.Text = staffInfoDict["staff_city"];
             lblAddressCounty.Text = staffInfoDict["staff_county"];
             lblAddressPostcode.Text = staffInfoDict["staff_postcode"];
+            //Employment
+            lblEmploymentStaffID.Text = staffInfoDict["staff_id"];
+            lblEmploymentJobTitle.Text = staffInfoDict["jobposition_name"];
+            lblEmploymentContractType.Text = staffInfoDict["staff_contract_type"];
+            lblEmploymentBranch.Text = staffInfoDict["staff_branch"];
+            //Contact Info
+            lblContactInfoEmailAddress.Text = staffInfoDict["staff_email"];
+            lblContactInfoMobileNumber.Text = staffInfoDict["staff_mobile_number"];
+            lblContactInfoHomeNumber.Text = staffInfoDict["staff_home_number"];
+            //Employment Info
+            lblEmploymentInfoDoB.Text = staffInfoDict["staff_DoB"];
+            lblEmploymentInfoGender.Text = staffInfoDict["staff_gender"];
+            lblEmploymentInfoCurrentlyEmployed.Text = staffInfoDict["staff_employed"];
+            //Payment Details
+            lblPaymentDetailsNILetter.Text = staffInfoDict["staff_NI_letter"];
+            lblPaymentDetailsTaxCode.Text = staffInfoDict["staff_tax_code"];
+            lblPaymentDetailsWorksNumber.Text = staffInfoDict["staff_works_number"];
         }
     }
 }
